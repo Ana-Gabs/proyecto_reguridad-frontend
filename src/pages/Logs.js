3f74004a4dacda24d541b0500ab8182fc137fe3f
Ana-Gabs <2022371069@uteq.edu.mx>
2025-04-03 15:07:14 -0600

    "graficas"

--- src/pages/Logs.js
+++ src/pages/Logs.js
@@ -1,8 +1,11 @@
 import React from "react";
 import Header from "../components/common/Header"; 
 import S1Level from "../components/graphics/s1Level";
+import S2Level from "../components/graphics/s2Level";
 import S1Status from "../components/graphics/s1Status";
+import S2Status from "../components/graphics/s2Status";
 import S1Time from "../components/graphics/s1Time";
+import S2Time from "../components/graphics/s2Time";
 import "../styles/Logs.css"; 
 
 const Logs = () => {
@@ -14,30 +17,30 @@ const Logs = () => {
         
         <div className="grid-container">
           <div className="chart-box">
-            <h3>Logs por Nivel</h3>
+            <h3>Logs por Nivel Servidor 1</h3>
             <S1Level />
           </div>
           <div className="chart-box">
-            <h3>Logs por Nivel</h3>
-            <S1Level />
+            <h3>Logs por Nivel Servidor 2</h3>
+            <S2Level />
           </div>
 
           <div className="chart-box">
-            <h3>Logs por Estado</h3>
+            <h3>Logs por Estado Servidor 1</h3>
             <S1Status />
           </div>
           <div className="chart-box">
-            <h3>Logs por Estado</h3>
-            <S1Status />
+            <h3>Logs por Estado Servidor 2</h3>
+            <S2Status />
           </div>
 
           <div className="chart-box">
-            <h3>Tiempo de Respuesta</h3>
+            <h3>Tiempo de Respuesta Servidor 1</h3>
             <S1Time />
           </div>
           <div className="chart-box">
-            <h3>Tiempo de Respuesta</h3>
-            <S1Time />
+            <h3>Tiempo de Respuesta Servidor 2</h3>
+            <S2Time />
           </div>
         </div>
       </div>
